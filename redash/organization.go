5f02aeab3a7120a3230c3286923bb8e1ad1c587a
Leandro Lorenzini <leandro@outlook.sg>
2022-03-28 16:24:57 +0800

    Changed type of AuthSamlEnabled

--- redash/organization.go
+++ redash/organization.go
@@ -22,23 +22,12 @@ import (
 // Organization struct
 type Organization struct {
 	AuthPasswordLoginEnabled	bool   `json:"auth_password_login_enabled,omitempty"`
-	AuthSamlEnabled           	string `json:"auth_saml_enabled,omitempty"`
+	AuthSamlEnabled           	bool   `json:"auth_saml_enabled,omitempty"`
 	AuthSamlType              	string `json:"auth_saml_type,omitempty"`
 	AuthSamlEntityId         	string `json:"auth_saml_entity_id,omitempty"`
 	AuthSamlMetadataUrl      	string `json:"auth_saml_metadata_url,omitempty"`
 	AuthSamlNameidFormat     	string `json:"auth_saml_nameid_format,omitempty"`
 	AuthSamlSsoUrl           	string `json:"auth_saml_sso_url,omitempty"`
-}
-
-// OrganizationCreatePayload struct
-type OrganizationCreatePayload struct {
-	AuthPasswordLoginEnabled	bool   `json:"auth_password_login_enabled"`
-	AuthSamlEnabled           	string `json:"auth_saml_enabled"`
-	AuthSamlType              	string `json:"auth_saml_type"`
-	AuthSamlEntityId         	string `json:"auth_saml_entity_id"`
-	AuthSamlMetadataUrl      	string `json:"auth_saml_metadata_url"`
-	AuthSamlNameidFormat     	string `json:"auth_saml_nameid_format"`
-	AuthSamlSsoUrl           	string `json:"auth_saml_sso_url"`
 }
 
 // GetOrganization returns the organization settings
